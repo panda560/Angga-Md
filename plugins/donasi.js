68b20b47d6d59e269d858078c3933c116f7e9118
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-07-24 07:17:13 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ┏──「 Donasi • Dana 」─⬣
-│ • Pulsa : 085850539404
-│ • Dana : 085850539404
+│ • Pulsa : +62 819-5929-3465
+│ • Gopay : +62 812-7367-7810
 ╰────────⬣
 ┏──「 *NOTE* 」─⬣
-│ > Ingin donasi? wa.me/6285850539404
+│ > Ingin donasi? wa.me/6281959293465
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
